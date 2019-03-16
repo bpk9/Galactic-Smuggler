633894cd8c95269a6d1d690701536c4b6b11b870
Austin Schaefer <Schaefer@austins-macbook-pro-3.local>
2019-03-16 18:33:15 -0400

    more stuff

--- Galactic Smuggler/PlanetLaunchView.swift
+++ Galactic Smuggler/PlanetLaunchView.swift
@@ -11,5 +11,18 @@ import UIKit
 class PlanetLaunchView: UIView
 
 {
+    @IBAction func LeftPlanet(_ sender: Any) {
+        
+    }
+    
+    @IBAction func RightPlanet(_ sender: UIButton) {
+    }
+    
+    @IBAction func Launch(_ sender: Any) {
+    }
+    
+    @IBAction func Back(_ sender: Any) {
+    }
+
     
 }
