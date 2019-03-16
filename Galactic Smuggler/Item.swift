ad054b35d6e1c6cd690ec645ff2f74a709d4b997
Brian Kasper <bkasperr@gmail.com>
2019-03-16 16:56:19 -0400

    Initial Commit

---
+++ Galactic Smuggler/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Galactic Smuggler
+//
+//  Created by Brian Kasper on 3/16/19.
+//  Copyright © 2019 Brian P Kasper. All rights reserved.
+//
+
+class Item
+{
+    init()
+    {
+        
+    }
+}
