f9dad2b4be86c60fe0588ff018e368983cf561e8
Brian Kasper <bkasperr@gmail.com>
2019-03-16 18:34:47 -0400

    Views

--- Galactic Smuggler/PurchaseWindow.swift
+++ Galactic Smuggler/PurchaseWindow.swift
@@ -10,19 +10,69 @@ import UIKit
 
 class PurchaseWindow: UIView
 {
-    var lessButton: UIButton = {
-        
-    };
-    var item: Item;
-    var moreButton: UIButton;
+    // view elements
+    @IBOutlet var lessButton: UIButton!
+    @IBOutlet var itemPicture: UIImageView!
+    @IBOutlet var itemPrice: UILabel!
+    @IBOutlet var moreButton: UIButton!
+    @IBOutlet var itemCount: UILabel!
+    
+    // other variables
+    var itemCounter: Int = 0;
     
     init(frame: CGRect, item: Item)
     {
-        self.item = item;
         super.init(frame: frame);
+        loadUI(item: item)
     }
     
     required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder);
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    
+    
+    // init function
+    class func instanceFromNib() -> UIView {
+        return UINib(nibName: "PurchaseWindow", bundle: nil).instantiate(withOwner: self, options: nil)[0] as! UIView
+    }
+    
+    // load UI elements
+    func loadUI(item: Item)
+    {
+        self.itemPicture.image = item.getImage();
+        self.itemPrice.text = String(item.getPrice());
+    }
+    
+    // called when more button is clicked
+    @IBAction func moreButtonAction(_ sender: Any)
+    {
+        addItem();
+    }
+    
+    // called when less button is clicked
+    @IBAction func lessButtonAction(_ sender: Any)
+    {
+        removeItem();
+    }
+    
+    func addItem() {
+        itemCounter += 1;
+        self.itemCount.text = String(itemCounter);
+        self.lessButton.isHidden = false;
+    }
+    
+    func removeItem() {
+        if(itemCounter > 1)
+        {
+            itemCounter -= 1;
+            self.itemCount.text = String(itemCounter);
+            if (itemCounter == 0) { self.lessButton.isHidden = true; }
+        }
+        else
+        {
+            // no-op
+        }
+    }
+    
 }
