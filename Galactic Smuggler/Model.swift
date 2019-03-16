4cb131678bc48c3698ef1ce67626d78e96d8eaf3
Austin Schaefer <Schaefer@austins-macbook-pro-3.local>
2019-03-16 19:27:28 -0400

    hello

---
+++ Galactic Smuggler/Model.swift
@@ -0,0 +1,30 @@
+//
+//  Model.swift
+//  Galactic Smuggler
+//
+//  Created by Austin on 3/16/19.
+//  Copyright © 2019 Brian P Kasper. All rights reserved.
+//
+
+import UIKit
+
+class Model{
+    
+    //Pictures:
+    let Earthpic = UIImage(named: "EarthSmuggle.png")
+    let Marspic = UIImage(named: "MarsSmubble.png")
+    
+    
+    //Item Arrays:
+    let EarthItems = ["Humans"]
+    let MarsItems = ["Iron", "Tungsten", "Aluminum", "Gold", "Lithium", "Niobium"]
+    
+    
+    func InitializePlanets() {
+        var Earth = Planet("Earth", Earthpic,
+                           
+    }
+    
+    
+    
+}
