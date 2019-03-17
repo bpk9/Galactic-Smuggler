ffc2e0d42a4d94479c802f2579cac38184c9f26a
Austin Schaefer <Schaefer@austins-macbook-pro-3.local>
2019-03-17 04:18:05 -0400

    gi

--- Galactic Smuggler/Model.swift
+++ Galactic Smuggler/Model.swift
@@ -18,20 +18,20 @@ class Model{
     static let Marspic = UIImage(named: "MarsSmuggle.png") as! UIImage
     static let Goldpic = UIImage(named: "GOld.png") as! UIImage
     static let Waterpic = UIImage(named: "waterdrum.png") as! UIImage
-    static let Niodiumpic = UIImage(named: "Niodium.png") as! UIImage
+    static let Niobiumpic = UIImage(named: "Niodium.png") as! UIImage
     
     
     
     //Items:
     static var Gold : Item = Item(name: "Gold Ingot", starting_price: 40.0, image: Goldpic)
     static var Water : Item = Item(name: "Water Barrel", starting_price: 20.0, image: Waterpic)
-    //static var Niobium : Item = Item(name: "Niobium Ore", image: Niobiumpic)
+    static var Niobium : Item = Item(name: "Niobium Ore", starting_price: 60.0 image: Niobiumpic)
     //var Gold = Item(name: "Gold", image: )
     
     
     
     static let EarthItems: [Item] = [Gold, Water]
-    static let MarsItems: [Item] = [Gold]
+    static let MarsItems: [Item] = [Gold, Niobium]
     
     static var current_planet: Planet! = Earth;
     
